5f23826f37ec9c646d9f7ab0b6d5893773e012a0
Osama Ahmed <osamaahmed.sus@gmail.com>
2022-06-20 16:42:24 +0200

    added helmet middleware and parsing using json

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express, { Application } from "express";
 import cors from "cors";
 import morgan from "morgan";
+import helmet from "helmet";
 import routes from "./routes";
 
 // express server instance
 const app: Application = express();
+// middleware for parsing json & urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // enable cors
 app.use(cors());
+// HTTP security middleware
+app.use(helmet());
+
 // HTTP request logger middleware
 app.use(morgan("dev"));
 
